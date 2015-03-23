6cc598f86246fef83b5c3777d567d89b2ccb921f
allegra15 <allegraabramo@gmail.com>
2015-03-23 19:23:22 -0400

    add reset button

--- js/script.js
+++ js/script.js
@@ -1,17 +1,39 @@
 function tipCalculate () 
 {
 	var totalBill = document.getElementById("bill").value;
-
-var percBtns = document.getElementsByClassName("tip-btn");
-
-for(var i=0; i<percBtns.length; i++) 
-{
-	if(percBtns[i].checked)
+	var percBtns = document.getElementsByClassName("tip-btn");
+	
+	for(var i=0; i<percBtns.length; i++) 
+	{
+		if(percBtns[i].checked)
 		var tipPerc = percBtns[i].value;
+	}
+
+//! means if false
+
+	if(!isNaN(totalBill))
+	{
+		var tipTotal = Math.floor(totalBill * tipPerc);
+		var tipBox = document.getElementById("tipamount");
+		tipBox.innerHTML = "$"+tipTotal;
+	} 
+	else {
+		alert("Please enter numbers only");
+		resetCalc();
+	}
 }
 
-var tipTotal = totalBill * tipPerc;
-var tipBox = document.getElementById("tipamount");
-tipBox.innerHTML = "$"+tipTotal;
+//set bill amount to 0
+function resetCalc(){
+  document.getElementById('bill').value = '';
 
+//reset tip pct buttons
+  var percBtns = document.getElementsByClassName('tip-btn');
+  for(var i=0; i < percBtns.length; i++){
+    percBtns[i].checked = false;
+  }
+//reset tip amount
+  var tipBox = document.getElementById('tipamount');
+  tipBox.innerHTML = '$0.00';
 }
+
